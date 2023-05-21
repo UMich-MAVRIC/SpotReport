f632fd3466bec9bf046aa38a2ce49ae7f69a4727
Wonse Jo <wonsu0513@gmail.com>
2023-05-20 23:13:33 -0400

    updated lsl names.

--- src/pylsl_function.py
+++ src/pylsl_function.py
@@ -9,7 +9,7 @@ outlet_spt_mouse_pos = StreamOutlet(info_spt_mouse_pos)
 info_spt_mouse_btn = StreamInfo("spt_mouse_btn", 'mouse_button', 1, IRREGULAR_RATE, cf_string, 'spotreport_gui')
 outlet_spt_mouse_btn = StreamOutlet(info_spt_mouse_btn)
 
-info_spt_processing_time = StreamInfo("spt_processing_time", 'task_time', 2, IRREGULAR_RATE, cf_float32, 'spotreport_gui') #unit: seconds
+info_spt_processing_time = StreamInfo("spt_task_time", 'task_time', 2, IRREGULAR_RATE, cf_float32, 'spotreport_gui') #unit: seconds
 outlet_spt_processing_time = StreamOutlet(info_spt_processing_time)
 spt_processing_channels = info_spt_processing_time.desc().append_child("channels")
 spt_processing_channels.append_child("channel")\
@@ -21,15 +21,15 @@ spt_processing_channels.append_child("channel")\
         .append_child_value("unit", "second")\
         .append_child_value("type", "time")
 
-info_spt_task_scores = StreamInfo("spt_task_score", 'score', 3, IRREGULAR_RATE, cf_float32, 'spotreport_gui') #unit: seconds
+info_spt_task_scores = StreamInfo("spt_task_accuracy", 'task_accuracy', 3, IRREGULAR_RATE, cf_float32, 'spotreport_gui') #unit: seconds
 outlet_spt_task_scores = StreamOutlet(info_spt_task_scores)
 spt_task_scores_channels = info_spt_task_scores.desc().append_child("channels")
 spt_task_scores_channels.append_child("channel")\
-        .append_child_value("name", "correct")\
+        .append_child_value("name", "correct_counts")\
         .append_child_value("unit", "number")\
         .append_child_value("type", "int")
 spt_task_scores_channels.append_child("channel")\
-        .append_child_value("name", "incorrect")\
+        .append_child_value("name", "incorrect_counts")\
         .append_child_value("unit", "number")\
         .append_child_value("type", "int")
 spt_task_scores_channels.append_child("channel")\
